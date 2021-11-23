950344e8d75e662ceadeaca34187857cce645623
MollyCK <carlesm@tcd.ie>
2021-11-23 13:51:13 +0000

    testing Scotty

--- shapeServer/app/Main.hs
+++ shapeServer/app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 
+import Web.Scotty
+
 main :: IO ()
-main = someFunc
+main = scotty 3000 ( do
+    get "/" ( do
+        html "Hello World"
+     )
+
+
+    get "/greet/:name" ( do
+        name <- param "name"
+        html (
+            mconcat [ "Hey there ", name]
+         )
+     )
+ )
