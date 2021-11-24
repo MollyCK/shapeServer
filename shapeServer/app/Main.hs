1a7a4c277e22b498b2e1edb7794d08818b2bcc13
MollyCK <carlesm@tcd.ie>
2021-11-24 19:11:04 +0000

    Testing out basic drawings

--- shapeServer/app/Main.hs
+++ shapeServer/app/Main.hs
@@ -1,21 +1,52 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Shapes
+import Render (render,defaultWindow)
 
 import Web.Scotty
 
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html.Renderer.Text as R
+
+{-
 main :: IO ()
 main = scotty 3000 ( do
     get "/" ( do
         html "Hello World"
      )
 
-
     get "/greet/:name" ( do
         name <- param "name"
         html (
-            mconcat [ "Hey there ", name]
+            mconcat ["Hey there ", name]
          )
      )
  )
+-}
+
+exampleDrawing = [(scale (point 0.5 0.25) <+> translate (point 1.2 0.4), circle)]
+
+
+main = render defaultWindow exampleDrawing
+
+{-
+-- Sample code from lectures, useful as syntax & structure examples
+response :: Text -> Text
+response n = do 
+    R.renderHtml ( do 
+        H.h1 ("Hello " >> H.toHtml n)
+     )
+
+longresponse :: Text -> Text
+longresponse n = do
+    R.renderHtml ( do
+        H.head (H.title "Welcome page")
+        H.body ( do
+            H.h1 "Welcome!"
+            H.p ("Welcome to my Scotty app" >> H.toHtml n)
+            )
+        )
+
+-}
